fbc1087fe85a7afd30831450b2d4376fbf77668a
Kiyohiro Honda <kiyohiro.honda@nex.nikkei.com>
2022-07-26 21:43:37 +0900

    update: temporally update ６章が終わった状態

---
+++ quiz/QuizViewController.swift
@@ -0,0 +1,32 @@
+//
+//  QuizViewController.swift
+//  quiz
+//
+//  Created by Kiyohiro Honda on 2022/07/26.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
+    var nameText: String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- quiz/ViewController.swift
+++ quiz/ViewController.swift
@@ -10,10 +10,19 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         self.label.text = "hello!"
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let quiqViewController = segue.destination as? QuizViewController{
+            if let text = self.nameTextField.text {
+                quiqViewController.nameText = text
+            }
+        }
     }
 
     @IBAction func pressButton(_ sender: Any) {

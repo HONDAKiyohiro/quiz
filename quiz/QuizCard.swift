d88e62c247e832212f91c5fd6eeb683c0073f2c8
Kiyohiro Honda <kiyohiro.honda@nex.nikkei.com>
2022-07-29 11:56:44 +0900

    update: temporally update 7.1章の最初の手順の６まで完了した

---
+++ quiz/QuizCard.swift
@@ -0,0 +1,20 @@
+//
+//  QuizCard.swift
+//  quiz
+//
+//  Created by Kiyohiro Honda on 2022/07/29.
+//
+
+import UIKit
+
+class QuizCard: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
